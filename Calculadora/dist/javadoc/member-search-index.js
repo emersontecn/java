04e52f5b52700788e4992aa8de53fdd73d8ffc89
emersontecn <emerson0712002@gmail.com>
2023-08-08 17:29:42 -0300

    confirmado

---
+++ Calculadora/dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"calculadora","c":"Tela","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"calculadora","c":"Tela","l":"Tela()","u":"%3Cinit%3E()"}];updateSearchResults();
